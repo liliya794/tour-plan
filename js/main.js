2c188dc02513007216bf309f80fed4216d5b22d9
liliya794 <liliyabutrimova@gmail.com>
2020-07-20 18:24:09 +0300

    create: added swiper-slider in hotel section

---
+++ js/main.js
@@ -0,0 +1,20 @@
+var mySwiper = new Swiper(".swiper-container", {
+  // Optional parameters
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".slider-button--next",
+    prevEl: ".slider-button--prev",
+  },
+  effect: "fade",
+
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
+
+  autoplay: {
+    delay: 3000,
+  },
+});
